647b60099f97c8a0f9abd7486f63dc527e5c5591
Ranjith Kumar U <ranjithudayakumar3@gmail.com>
2020-07-10 23:28:06 +0530

    UI

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+<<<<<<< HEAD
 import logo from './logo.svg';
+=======
+import { Header } from './components/Header';
+import { Balance } from './components/Balance';
+import { IncomeExpenses } from './components/IncomeExpenses';
+import { TransactionList } from './components/TransactionList';
+import { AddTransaction } from './components/AddTransaction';
+>>>>>>> d727d9b... UI
 import './App.css';
 
 function App() {
   return (
+<<<<<<< HEAD
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -19,6 +28,16 @@ function App() {
           Learn React
         </a>
       </header>
+=======
+    <div>
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
+      </div>
+>>>>>>> d727d9b... UI
     </div>
   );
 }
