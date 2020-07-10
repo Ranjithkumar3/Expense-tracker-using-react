2a3a061c5ac1acf63c2f7baf8cf2bec90a843c55
Ranjith Kumar U <ranjithudayakumar3@gmail.com>
2020-07-10 23:34:49 +0530

    Second Commit

--- src/App.js
+++ src/App.js
@@ -1,34 +1,14 @@
 import React from 'react';
-<<<<<<< HEAD
-import logo from './logo.svg';
-=======
+
 import { Header } from './components/Header';
 import { Balance } from './components/Balance';
 import { IncomeExpenses } from './components/IncomeExpenses';
 import { TransactionList } from './components/TransactionList';
 import { AddTransaction } from './components/AddTransaction';
->>>>>>> d727d9b... UI
 import './App.css';
 
 function App() {
   return (
-<<<<<<< HEAD
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-=======
     <div>
       <Header />
       <div className="container">
@@ -37,7 +17,6 @@ function App() {
         <TransactionList />
         <AddTransaction />
       </div>
->>>>>>> d727d9b... UI
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-=======
 import App from './App';
 
->>>>>>> d727d9b... UI
 
 ReactDOM.render(
   <React.StrictMode>
@@ -15,11 +9,4 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-<<<<<<< HEAD
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
-=======
->>>>>>> d727d9b... UI
